4e0fde20c310bd8baa0e41213ca804137bece68a
mbasilyan <mbasilyan@gmail.com>
2014-11-23 00:06:26 -0800

    Got the v1 of the nodes finished

--- src/com/basilyan/keyvalnode/KeyValNode.java
+++ src/com/basilyan/keyvalnode/KeyValNode.java
@@ -1,11 +1,14 @@
 package com.basilyan.keyvalnode;
 
+import java.net.ServerSocket;
+import java.io.IOException;
+import java.util.HashMap;
+
 /**
  * This represents the KV node. It launches.
  * Initializes a KV map. Listens for connections 
  * and adds/subtracts KV pairs. This is going to be simple & dumb by design.
  * 
- * Also needs to manage a queue?
  * 
  * @author Michael Basilyan
  *
@@ -13,8 +16,26 @@ package com.basilyan.keyvalnode;
 public class KeyValNode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HashMap<String, String> hm = new HashMap<String, String>();
 
+		if (args.length != 1) {
+			System.err.println("Usage: java KeyValNode <port number>");
+			System.exit(1);
+		}
+
+		int portNumber = Integer.parseInt(args[0]);
+		boolean listening = true;
+
+		try { 
+			ServerSocket serverSocket = new ServerSocket(portNumber);
+			while (listening) {
+				new KeyValNodeRequestHandler(serverSocket.accept(), hm).start();
+			}
+		} catch (IOException e) {
+			System.err.println("Could not listen on port " + portNumber);
+			System.exit(-1);
+		}
 	}
 
 }
+
---
+++ src/com/basilyan/keyvalnode/KeyValNodeRequestHandler.java
@@ -0,0 +1,74 @@
+package com.basilyan.keyvalnode;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class KeyValNodeRequestHandler extends Thread {
+
+
+	 final static String CRLF = "\r\n";
+	 private Socket socket = null;
+	 private HashMap<String, String> hm = null;
+	 
+	public KeyValNodeRequestHandler(Socket socket, HashMap hm)
+	{
+		super("KeyValNodeRequestHandlerThread_" + socket.getInetAddress().toString());
+		this.socket = socket;
+		this.hm = hm;
+		System.out.println("Connected to: " + socket.getInetAddress() + " on " + socket.getLocalPort());
+	}
+	
+	@Override
+	public void run() {
+		/*
+		 * Inspired by: 
+		 * https://github.com/grahamking/Key-Value-Polyglot/blob/master/Memg.java
+		 * 
+		 */
+		try {
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			while(true) {
+				String inputLine = in.readLine();
+				if(inputLine.equals("")) break;
+				String[] inputLineSplit = inputLine.split(" ");
+				System.out.println("Recieved get command:" + inputLine);
+				String cmd = inputLineSplit[0];
+				if(cmd.toLowerCase().equals("get")) {
+					String key = inputLineSplit[1];
+					String val;
+					if(hm.containsKey(key)) {
+						val = hm.get(key);
+						out.printf("VALUE %s 0 %d\r\n", key, val.length());
+						out.printf("%s\r\n",val);
+					}
+					out.printf("END\r\n");
+				} else if(cmd.toLowerCase().equals("set")) {
+					System.out.println("Recieved set command:" + inputLine);
+					String key = inputLineSplit[1];
+					int length = Integer.parseInt(inputLineSplit[4]);
+					char[] buf = new char[length + 2]; //This is not 100% correct for handling unicode bytes, but it's fine here for the benchmark test
+					int index = 0;
+					while (index < buf.length) {
+						int len = in.read(buf, index, buf.length - index);
+						if (len == -1)
+							break;
+						index += len;
+					}
+					String val = new String(buf, 0, length);
+					hm.put(key, val);
+					out.printf("STORED\r\n");
+				} else {
+					System.out.println("Unknown cmd: " + cmd);
+					break;
+				}
+				out.flush();
+			}
+		}
+		catch(Exception ex) {
+			System.out.println(ex);
+		}
+	}
+
+}
